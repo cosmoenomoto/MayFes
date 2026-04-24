18cce40340233e362ace1a409ec9f56b93f53997
cosmoenomoto <cosmoenomoto@g.ecc.u-tokyo.ac.jp>
2026-04-24 11:26:39 +0900

    Fix: Allow Render host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+  config.hosts << "mayfes.onrender.com"
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
