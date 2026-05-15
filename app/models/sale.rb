d58d07c561338fd681bb5a306eda685fa646c1d9
cosmoenomoto <cosmoenomoto@g.ecc.u-tokyo.ac.jp>
2026-05-16 08:41:09 +0900

    option追加

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,8 +1,8 @@
 class Sale < ApplicationRecord
-  enum item_type: { item_a: 0, item_b: 1 }
+  enum item_type: { item_a: 0, item_b: 1, option: 2 }
   enum gender: { male: 0, female: 1, other: 2 }
   enum age_group: { young: 0, adult: 1 }
-  enum item_type: { item_a: 0, item_b: 1, option: 2 }
+
 
   # 日本語表示用のメソッド（Viewをスッキリさせるため）
   def item_type_jp
